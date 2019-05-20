498283fc31f1583e096102938ea6d1e253833c00
Your Name <tfikrie@yahoo.com>
2019-05-19 22:18:44 -0400

    About page body started... the framworkand animation of the carousel completed

---
+++ JS/about.js
@@ -0,0 +1,17 @@
+var index = 0;
+carouselShow();
+
+function carouselShow() {
+  var i;
+  var carousel = document.querySelectorAll('.carousel');
+  
+  for (i = 0; i < carousel.length; i++) {
+    carousel[i].style.display = "none";  
+  }
+  index++;
+  if (index > carousel.length) {index = 1}    
+  
+  carousel[index-1].style.display = "block";  
+
+  setTimeout(carouselShow, 2000); 
+}
